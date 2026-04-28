a7f3624bb06ddbba751861f1b8e28135eb2314bf
Nuggu Phani Harsha <phaniharsharam@gmail.com>
2026-04-28 20:30:06 +0530

    manage subjects functionality on teachers screen

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from src.components.header import header_dashboard
 from src.ui.base_layout import style_base_layout, style_background_dashboard
-from src.database.db import check_teacher_exists, create_teacher, teacher_login
+from src.database.db import check_teacher_exists, create_teacher, teacher_login, get_teacher_subjects
+from src.components.dialog_create_subject import create_subject_dialog
+from src.components.subject_card import subject_card
 
 def teacher_screen():
 
@@ -67,7 +69,38 @@ def teacher_tab_take_attendance():
     st.header('Take AI Attendance')
 
 def teacher_tab_manage_subjects():
-    st.header('Take AI Attendance')
+    teacher_id = st.session_state.teacher_data['teacher_id']
+    col1, col2 = st.columns(2)
+    with col1:
+        st.header('Manage Subjects', width='stretch')
+
+    with col2:
+        if st.button('Create New Subject', width='stretch'):
+            create_subject_dialog(teacher_id)
+
+
+    # LIST all SUBJECTS
+    subjects = get_teacher_subjects(teacher_id)
+    if subjects:
+        for sub in subjects:
+            stats = [
+                ("🫂", "Students", sub['total_students']),
+                ("🕰️", "Classes", sub['total_classes']),
+            ]
+        def share_btn():
+            if st.button(f"Share Code: {sub['name']}", key=f"share_{sub['subject_code']}", icon=":material/share:"):
+                share_subject_dialog(sub['name'], sub['subject_code'])
+            st.space()
+
+        subject_card(
+            name = sub['name'],
+            code = sub['subject_code'],
+            section = sub['section'],
+            stats=stats,
+            footer_callback=share_btn
+        )
+    else:
+        st.info("NO SUBJECTS FOUND. CREATE ONE ABOVE")
 
 def teacher_tab_attendance_records():
     st.header('Take AI Attendance')
