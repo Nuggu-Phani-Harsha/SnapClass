1102a3fde35ddb8113878cf9072f1937b8c8feb2
Nuggu Phani Harsha <phaniharsharam@gmail.com>
2026-04-30 21:16:29 +0530

    unenroll student from a subject

--- src/database/db.py
+++ src/database/db.py
@@ -61,3 +61,7 @@ def  enroll_student_to_subject(student_id, subject_id):
     data = {'student_id': student_id, "subject_id": subject_id}
     response= supabase.table('subject_students').insert(data).execute()
     return response.data
+
+def  unenroll_student_to_subject(student_id, subject_id):
+    response= supabase.table('subject_students').delete().eq('student_id', student_id).eq('subject_id', subject_id).execute()
+    return response.data
